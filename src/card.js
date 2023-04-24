599ab576903853a689973ac38db1a929cc2239b7
Miguel Figuera <miguelqui725@gmail.com>
2023-04-24 18:44:47 -0400

    Create components, add logic to components
    This commits does the next actions:
     - create helper to save the array of characters.
     - creates a function called randomCharacters to retrieve
        the characters in a random order and exports this function.
     - adds styling using bootstrap to almost every part of the
        dom elements.
     - creates header and cards components.
     - creates all the functions on app.js to modify the states.
     - pass the functions as props to child components
     - pass the states as props to child components.

--- src/card.js
+++ src/card.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 
-function Card({data}){
+function Card(props){
 
 
   return (
-    <div className="card" style={{width: '18rem'}} >
-      <img src={data.img} className="card-img-top" alt="Imagen of Star Trek Character" />
+    <div className="card me-3 ms-3 mt-3 mb-3" style={{width: '18rem'}} >
+      <img src={props.data.img} className="card-img-top" alt="Imagen of Star Trek Character" />
       <div className="card-body">
-        <h5 className="card-title">{data.name}</h5>
-        <button className="btn btn-primary">
+        <h5 className="card-title">{props.data.name}</h5>
+        <button className="btn btn-primary" onClick={()=>props.handleClick(props.data.id)}>
           Choose me!
         </button>
       </div>
--- src/header.js
+++ src/header.js
@@ -1,19 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Card } from "./card";
 
-
-function Header(){
+function Header(props){
 
 
     return(
-        <div className="container">
+        <div className="container align-content-center d-flex p-2 justify-content " style={{color:'rgb(202,202,202)'}}>
         <div className="page-header">
             <h1>Memory Game</h1>
         </div>
-
-        <div className="container-sm">
-            <p>Current Score: {}</p>
-            <p>Best Score: {}</p>
+        <div></div>
+        <div className="container-sm d-flex ">
+            <p className="me-3">Current Score: {props.score}</p>
+            <p className="ms-3">Best Score: {props.bestScore}</p>
         </div>
         </div>
 
